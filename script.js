cd5f3be900344db87f0b8853c49b5f1d240a1602
abdusaydin <abdus47ay@gmail.com>
2026-03-28 01:27:24 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateClock(){
+let now=new Date();
+document.getElementById("clock").innerText=now.toLocaleTimeString("tr-TR");
+}
+setInterval(updateClock,1000);
+
+let images=["img1.jpg","img2.jpg","img3.jpg"];
+let i=0;
+
+function changeSlide(){
+i++;
+if(i>=images.length)i=0;
+document.getElementById("slide").src=images[i];
+}
+setInterval(changeSlide,5000);
